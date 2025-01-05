4ef086206aae178a7a42df83553ea6d41e6cc690
nafrasia <nafrasia@uwo.ca>
2025-01-04 21:19:10 -0500

    sigmoid function added to utility header file

--- src/utility.py
+++ src/utility.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def quicksort(X):
     '''
     Performs quick sort
@@ -20,3 +22,6 @@ def quicksort(X):
         middle = [pivot]
         right = [x for x in X if x > pivot]
         return quicksort(left) + middle + quicksort(right)
+
+def sigmoid(X):
+    return 1/(1+np.exp(-X))
